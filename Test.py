4bf0752521d44489e4f3a53291a7bc95ff86d0b5
Daniel3131 <71692327+TheDaniel3131@users.noreply.github.com>
2022-11-30 12:52:39 +0800

    Delete Test.py

--- Test.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-
-url = YouTube("https://www.youtube.com/watch?v=2G4MyJL2Xw8")
-
-mp4_files = url.streams.filter(file_extension="mp4")
-
-mp4_369p_files = mp4_files.get_by_resolution("1080p")
-
-mp4_369p_files.download("D:\Git Github\DownloadYouTubeVideos")
